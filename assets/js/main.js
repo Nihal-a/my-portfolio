a8ce651d053427e6a3f16b039864c554e8b1719b
Nihal A <nihalavulan1@gmail.com>
2021-05-28 17:42:00 +0530

    Skills Section Completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,3 +28,25 @@ function linkAction() {
   navMenu.classList.remove("show-menu");
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+
+//Skills view open/close
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills(){
+  let itemsClass = this.parentNode.className
+
+  for(i = 0; i<skillsContent.length;i++){
+    
+    skillsContent[i].className = 'skills__content skills__close'
+    
+  }
+  if(itemsClass=== 'skills__content skills__close'){
+    this.parentNode.className = 'skills__content skills__open'
+  }
+}
+
+skillsHeader.forEach((el)=>{
+  el.addEventListener('click',toggleSkills)
+})
